05eb54f2a09a4f5cc8bc51443b15031619041468
V4Vince <vince.guoo@gmail.com>
2017-11-13 02:16:20 -0500

    Adds user to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create({
+  email: 'dummy',
+  password: 'qwer' })
 
 about = AboutSection.create({
+  user: user,
   title: 'About Us',
   description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
 
 productSec = ProductSection.create({
+  user: user,
   title: 'Products',
   description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
 
 chefSection = ChefSection.create({
+  user: user,
   title: 'Chef',
   description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
 
 newsSec = NewsSection.create({
+  user: user,
   title: 'News',
   description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
 
 contact = Contact.create({
+  user: user,
   title: 'Get in touch with us',
   description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as op' })
 # history = History.create({
