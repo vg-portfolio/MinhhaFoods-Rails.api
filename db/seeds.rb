693a9ee83435e99c3e9292813fc1b11d9af197b6
V4Vince <vince.guoo@gmail.com>
2017-11-13 02:07:21 -0500

    Adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+about = AboutSection.create({
+  title: 'About Us',
+  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
+
+productSec = ProductSection.create({
+  title: 'Products',
+  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
+
+chefSection = ChefSection.create({
+  title: 'Chef',
+  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
+
+newsSec = NewsSection.create({
+  title: 'News',
+  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to' })
+
+contact = Contact.create({
+  title: 'Get in touch with us',
+  description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as op' })
+# history = History.create({
+#   title: 'History',
+#   description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters' })
